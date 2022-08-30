5ef88789f32bd692e0d265c2b6c03a10f554af89
Prololden <deniz.mamedov2020@mail.ru>
2022-08-30 18:01:14 +0300

    Createx website

---
+++ JS/javascript.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click', () => {
+        document.getElementById('123').style.display = 'block';
+        document.getElementById('123').style.position = 'relativ';
+});
+document.getElementById('btn_2').addEventListener('click', () => {
+        document.getElementById('123').style.display = 'none';
+});
